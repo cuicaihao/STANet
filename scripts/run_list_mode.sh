6fb7204c10db50a6ff626eafbe1c1abd84754ed3
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2022-05-22 10:31:16 +1000

    Review code on 2022 May 22

--- scripts/run_list_mode.sh
+++ scripts/run_list_mode.sh
@@ -1,4 +1,3 @@
-
 # List mode
 list=train
 lr=0.001
@@ -6,4 +5,4 @@ dataset_mode=list
 dataroot=path-to-dataroot
 name=project_name
 #
-python ./train.py --num_threads 4 --display_id 0 --dataroot ${dataroot} --val_dataroot ${dataroot} --save_epoch_freq 1 --niter 100 --angle 25 --niter_decay 100  --display_env FAp0 --SA_mode PAM --name $name --lr $lr --model CDFA --batch_size 4 --dataset_mode $dataset_mode --val_dataset_mode $dataset_mode --split $list --load_size 256 --crop_size 256 --preprocess resize_rotate_and_crop
+python ./train.py --num_threads 4 --display_id 0 --dataroot ${dataroot} --val_dataroot ${dataroot} --save_epoch_freq 1 --niter 100 --angle 25 --niter_decay 100 --display_env FAp0 --SA_mode PAM --name $name --lr $lr --model CDFA --batch_size 4 --dataset_mode $dataset_mode --val_dataset_mode $dataset_mode --split $list --load_size 256 --crop_size 256 --preprocess resize_rotate_and_crop
--- train.py
+++ train.py
@@ -25,6 +25,7 @@ def seed_torch(seed=2019):
 # seed_torch(2019)
 ifSaveImage = False
 
+
 def make_val_opt(opt):
 
     val_opt = copy.deepcopy(opt)
@@ -32,10 +33,13 @@ def make_val_opt(opt):
     # hard-code some parameters for test
     val_opt.num_threads = 0   # test code only supports num_threads = 1
     val_opt.batch_size = 4    # test code only supports batch_size = 1
-    val_opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    val_opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    val_opt.serial_batches = True
+    # no flip; comment this line if results on flipped images are needed.
+    val_opt.no_flip = True
     val_opt.angle = 0
-    val_opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    # no visdom display; the test code saves the results to a HTML file.
+    val_opt.display_id = -1
     val_opt.phase = 'val'
     val_opt.split = opt.val_split  # function in jsonDataset and ListDataset
     val_opt.isTrain = False
@@ -65,18 +69,22 @@ def print_current_acc(log_name, epoch, score):
 
 def val(opt, model):
     opt = make_val_opt(opt)
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    # create a dataset given opt.dataset_mode and other options
+    dataset = create_dataset(opt)
     # model = create_model(opt)      # create a model given opt.model and other options
     # model.setup(opt)               # regular setup: load and print networks; create schedulers
 
-    web_dir = os.path.join(opt.checkpoints_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch))  # define the website directory
-    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+    web_dir = os.path.join(opt.checkpoints_dir, opt.name, '%s_%s' % (
+        opt.phase, opt.epoch))  # define the website directory
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (
+        opt.name, opt.phase, opt.epoch))
     model.eval()
     # create a logging file to store training losses
     log_name = os.path.join(opt.checkpoints_dir, opt.name, 'val_log.txt')
     with open(log_name, "a") as log_file:
         now = time.strftime("%c")
-        log_file.write('================ val acc (%s) ================\n' % now)
+        log_file.write(
+            '================ val acc (%s) ================\n' % now)
 
     running_metrics = AverageMeter()
     for i, data in enumerate(dataset):
@@ -90,28 +98,35 @@ def val(opt, model):
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         if ifSaveImage:
-            save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+            save_images(webpage, visuals, img_path,
+                        aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
 
     score = running_metrics.get_scores()
     print_current_acc(log_name, epoch, score)
     if opt.display_id > 0:
-        visualizer.plot_current_acc(epoch, float(epoch_iter) / dataset_size, score)
+        visualizer.plot_current_acc(
+            epoch, float(epoch_iter) / dataset_size, score)
     webpage.save()  # save the HTML
 
     return score[metric_name]
+
 
 metric_name = 'F1_1'
 
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    # create a dataset given opt.dataset_mode and other options
+    dataset = create_dataset(opt)
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
 
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
-    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
+    # create a model given opt.model and other options
+    model = create_model(opt)
+    # regular setup: load and print networks; create schedulers
+    model.setup(opt)
+    # create a visualizer that display/save images and plots
+    visualizer = Visualizer(opt)
     total_iters = 0                # the total number of training iterations
     miou_best = 0
     n_epoch_bad = 0
@@ -120,12 +135,15 @@ if __name__ == '__main__':
     time_log_name = os.path.join(opt.checkpoints_dir, opt.name, 'time_log.txt')
     with open(time_log_name, "a") as log_file:
         now = time.strftime("%c")
-        log_file.write('================ training time (%s) ================\n' % now)
+        log_file.write(
+            '================ training time (%s) ================\n' % now)
 
-    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
-        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
+        # the number of training iterations in current epoch, reset to 0 every epoch
+        epoch_iter = 0
         model.train()
        # miou_current = val(opt, model)
         for i, data in enumerate(dataset):  # inner loop within one epoch
@@ -137,22 +155,28 @@ if __name__ == '__main__':
             epoch_iter += opt.batch_size
             n_epoch = opt.niter + opt.niter_decay
 
-            model.set_input(data)         # unpack data from dataset and apply preprocessing
-            model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
+            # unpack data from dataset and apply preprocessing
+            model.set_input(data)
+            # calculate loss functions, get gradients, update network weights
+            model.optimize_parameters()
             if ifSaveImage:
                 if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                     save_result = total_iters % opt.update_html_freq == 0
                     model.compute_visuals()
-                    visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                    visualizer.display_current_results(
+                        model.get_current_visuals(), epoch, save_result)
             if total_iters % opt.print_freq == 0:   # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                visualizer.print_current_losses(
+                    epoch, epoch_iter, losses, t_comp, t_data)
                 if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+                    visualizer.plot_current_losses(
+                        epoch, float(epoch_iter) / dataset_size, losses)
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
-                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                print('saving the latest model (epoch %d, total_iters %d)' %
+                      (epoch, total_iters))
                 save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
                 model.save_networks(save_suffix)
 
@@ -160,22 +184,24 @@ if __name__ == '__main__':
 
         t_epoch = time.time()-epoch_start_time
         time_metric.update(t_epoch)
-        print_current_acc(time_log_name, epoch,{"current_t_epoch": t_epoch})
-
+        print_current_acc(time_log_name, epoch, {"current_t_epoch": t_epoch})
 
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
-            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+            print('saving the model at the end of epoch %d, iters %d' %
+                  (epoch, total_iters))
             model.save_networks('latest')
             miou_current = val(opt, model)
 
             if miou_current > miou_best:
                 miou_best = miou_current
                 epoch_best = epoch
-                model.save_networks(str(epoch_best)+"_"+metric_name+'_'+'%0.5f'% miou_best)
+                model.save_networks(str(epoch_best)+"_" +
+                                    metric_name+'_'+'%0.5f' % miou_best)
 
-
-        print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
-        model.update_learning_rate()                     # update learning rates at the end of every epoch.
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+        # update learning rates at the end of every epoch.
+        model.update_learning_rate()
 
     time_ave = time_metric.average()
     print_current_acc(time_log_name, epoch, {"ave_t_epoch": time_ave})
